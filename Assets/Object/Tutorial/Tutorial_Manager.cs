7637b0f75e0b979029ad5b404c61aebd666ce9ab
634_3 <k020g1146@g.neec.ac.jp>
2021-12-08 18:11:56 +0900

    チュートリアル基礎

---
+++ Assets/Object/Tutorial/Tutorial_Manager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Tutorial_Manager : MonoBehaviour
+{
+    [Header("Player")]
+    [SerializeField] GameObject Player_Prefab = null;
+
+    [Header("Object")]
+    [SerializeField] GameObject Ball_Prefab = null;
+    [SerializeField] GameObject Niddle_prefab1 = null;
+    [SerializeField] GameObject Niddle_prefab2 = null;
+    [SerializeField] GameObject Niddle_prefab3 = null;
+
+    [Header("Camera")]
+    [SerializeField] GameObject VCamera = null;
+    CameraControll cameraControll = default;
+
+    [Header("UI")]
+    [SerializeField] GameObject UI = null;
+
+    [Header("TimeLine")]
+    [SerializeField] PlayableDirector StartingCutScene = null;
+    GameState state = GameState.None;
+    
+
+    private void Awake()
+    {
+        Ball_Prefab.SetActive(false);
+        Niddle_prefab1.SetActive(false);
+        Niddle_prefab2.SetActive(false);
+        Niddle_prefab3.SetActive(false);
+        UI.SetActive(false);
+
+        cameraControll = VCamera.GetComponent<CameraControll>();
+        cameraControll.enabled = false;
+    }
+
+    void Update()
+    {
+        switch (state)
+        { 
+            //再生
+            case GameState.None:
+                if (StartingCutScene)
+                {
+                    StartingCutScene.Play();
+                }
+                state = GameState.Opening;
+                break;
+
+            //再生終了
+            case GameState.Opening:
+                if(StartingCutScene && StartingCutScene.state != PlayState.Playing)
+                {
+                    StartingCutScene.gameObject.SetActive(false);
+                    Instantiate(Player_Prefab, new Vector3(0.1262228f, 0.5f, -29.80061f), Player_Prefab.transform.rotation);
+                    ActiveOn();
+                    state = GameState.InGame;
+                }
+                else if (!StartingCutScene)
+                {
+                    Instantiate(Player_Prefab, new Vector3(0.1262228f, 0.5f, -29.80061f), Player_Prefab.transform.rotation);
+                    ActiveOn();
+                    state = GameState.InGame;
+                }
+                break;
+        }
+    }
+
+
+    //Object表示
+    private void ActiveOn()
+    {
+        Ball_Prefab.SetActive(true);
+        Niddle_prefab1.SetActive(true);
+        Niddle_prefab2.SetActive(true);
+        Niddle_prefab3.SetActive(true);
+        UI.SetActive(true);
+
+        cameraControll.enabled = true;
+    }
+}
+
+public enum GameState 
+{
+    //ゲーム起動
+    None,
+    //再生中
+    Opening,
+    //ゲーム中
+    InGame,
+}
+
